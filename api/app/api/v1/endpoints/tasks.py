8b1db70579b147eae7ab03f68ed41ffe45fab5d1
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-02-20 12:36:50 +0900

    APIエンドポイント定義ファイルの追加

---
+++ api/app/api/v1/endpoints/tasks.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+router = APIRouter()
--- api/main.py
+++ api/main.py
@@ -1,5 +1,33 @@
 import uvicorn 
 from at import app
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from app.api.v1.api import api_router
+from app.core import config
+
+app = FastAPI(title=config.PROJECT_NAME, openapi_url="/api/v1/openapi.json")
+
+# CORS
+origins = []
+
+# Set all CORS enabled origins
+if config.BACKEND_CORS_ORIGINS:
+    origins_raw = config.BACKEND_CORS_ORIGINS.split(",")
+    for origin in origins_raw:
+        use_origin = origin.strip()
+        origins.append(use_origin)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    ),
+
+app.include_router(api_router, prefix=config.API_V1_STR)
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8080, host="0.0.0.0",reload = True)
