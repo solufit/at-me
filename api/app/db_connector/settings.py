8b5321d7ee483f165b654066c6dca6f5d9a0dc7d
Yuta Takahashi <y.highbridge04@gmail.com>
2024-02-21 20:06:45 +0900

    db api init

---
+++ api/app/db_connector/settings.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine(
+    "postgresql+psycopsg2://postgre:postgre@db:5432/atme"
+)
+session = sessionmaker(
+    engine = engine,
+    autoflush=True
+)
