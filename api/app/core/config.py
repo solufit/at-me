d304cdf4d1bf6a0f26c1c546997a1c83b97da88d
Yuta Takahashi <y.highbridge04@gmail.com>
2024-02-21 09:08:00 +0900

    add descripion and version information

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -11,7 +11,9 @@ def getenv_boolean(var_name, default_value=False):
 
 API_V1_STR = "/v1"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8  # 60 minutes * 24 hours * 8 days = 8 days
-BACKEND_CORS_ORIGINS = os.getenv(
-    "BACKEND_CORS_ORIGINS"
+BACKEND_CORS_ORIGINS = "http://localhost:8080, {}".format(os.getenv(
+    "BACKEND_CORS_ORIGINS")
 )  # a string of origins separated by commas, e.g: "http://localhost, http://localhost:4200, http://localhost:3000, http://localhost:8080, http://dev.couchbase-project.com, https://stag.couchbase-project.com, https://couchbase-project.com, http://local.dockertoolbox.tiangolo.com"
-PROJECT_NAME = os.getenv("PROJECT_NAME","default")
+PROJECT_NAME = "at-me Internal API"
+DESCRIPTION = "at-me Internal API"
+VERSION = "0.0.1"
