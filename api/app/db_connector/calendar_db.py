9657a1632c5ffbab45e4f33fe612f4bdf8ef936c
Yuta Takahashi <y.highbridge04@gmail.com>
2024-02-23 19:44:58 +0900

    add event create function

--- api/app/db_connector/calendar_db.py
+++ api/app/db_connector/calendar_db.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import sessionmaker, Session
 
 
 from app.db_connector.settings import session as session_maker
+from db.models import Event
+from app.basemodel.common_model import Event as BaseEvent
     
     
     
@@ -23,6 +25,46 @@ class calendar_db():
         
     def __delattr__(self, __name: str) -> None:
         self.session.close()
+
+        
+    def create(
+        self,
+        localId = BaseEvent.localId,
+        id = BaseEvent.id,
+        calendarID = BaseEvent.calendarID,
+        htmlLink = BaseEvent.htmlLink,
+        starttime = BaseEvent.starttime,
+        endtime = BaseEvent.endtime,
+        etag = BaseEvent.etag,
+        note = BaseEvent.note,
+        Taskid = BaseEvent.Taskid,
+        
+    ):
+
+        model = Event(
+            localId = localId,
+            id = id,
+            calendarID = calendarID,
+            htmlLink = htmlLink,
+            starttime = starttime,
+            endtime = endtime,
+            etag = etag,
+            note = note,
+            Taskid = Taskid
+        )
+        
+        
+        self.session.add(model)        
+
+        self.session.commit()
+
+        
+        
+
+    
+
+        
+        
 
 
 
