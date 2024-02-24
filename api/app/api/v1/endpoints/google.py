3002ba20cb7a6c56672a4b7187147f5c0a1fd333
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-02-25 08:31:25 +0900

    feature: Googleカレンダーからのデータ取得の仮実装

--- api/app/api/v1/endpoints/google.py
+++ api/app/api/v1/endpoints/google.py
@@ -1,59 +1,45 @@
-from fastapi import APIRouter, Body, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
-from pydantic import BaseModel, Field
-from typing import List
+""" google.py  """
 import datetime
 import os
 import requests
+from fastapi import APIRouter, Depends, HTTPException
+
 
 from app.core.jwt import get_current_user
-from app.basemodel.common_model import Event
 
 router = APIRouter()
 
-AUTH_API = os.getenv('AUTH_API')
-SECURE_LOCK = os.getenv('SECURE_LOCK')
+AUTH_API = os.getenv("AUTH_API")
+SECURE_LOCK = os.getenv("SECURE_LOCK")
 
-@router.get(
-    "/calender",
-    description = "get events of the specified date"
-)
+
+@router.get("/calender", description="get events of the specified date")
 async def get_calender(
-    userlink: str,
-    date: datetime.date  = datetime.date(1992, 4, 27),
-    cred = Depends(get_current_user)
-    ) -> List[Event]:
-    access_token = requests.get(f'{AUTH_API}/oauth2/token?linkcode={userlink}&secure={SECURE_LOCK}')
+    userlink: str, date: datetime.date = datetime.date(1992, 4, 27), cred=Depends(get_current_user)
+):
+    """
+
+    Args:
+        userlink (str): _description_
+        date (datetime.date, optional): _description_. Defaults to datetime.date(1992, 4, 27).
+        cred (_type_, optional): _description_. Defaults to Depends(get_current_user).
+
+    Raises:
+        HTTPException: _description_
+
+    Returns:
+        _type_: _description_
+    """
+    print(cred['email'])
+    access_token = await requests.get(f"{AUTH_API}/oauth2/token?linkcode={userlink}&secure={SECURE_LOCK}", timeout=(3.0, 7.5))
     if access_token.status_code == 403:
         raise HTTPException(status_code=403)
     access_token = access_token.text
 
-    ## 
-    ## 現時刻以降の予定を「早い順」に並び替えて返却
-    testdata = [
-        {
-            "id": '',
-            "calemderID": '',
-            "htmlLink": '',
-            "starttime": datetime.datetime(2024,2,24,11,0),
-            "endtime": datetime.datetime(2024,2,24,23,0),
-            'duringtime': ((datetime.datetime(2024,2,24,23,0) - datetime.datetime(2024,2,24,11,0)).seconds) // 60,
-            "title": '@me フロントエンド開発',
-            "etag": '',
-            "note": 'ハッカソン開発',
-            "Taskid": ''
-        },
-        {
-            "id": '',
-            "calemderID": '',
-            "htmlLink": '',
-            "starttime": datetime.datetime(2024,2,24,19,0),
-            "endtime": datetime.datetime(2024,2,24,19,15),
-            'duringtime': ((datetime.datetime(2024,2,24,19,15) - datetime.datetime(2024,2,24,19,0)).seconds) // 60,
-            "title": 'MTG',
-            "etag": '',
-            "note": '全体ミーティング',
-            "Taskid": ''
-        }
-    ]
-    return testdata
+    res = requests.get(
+        f"https://www.googleapis.com/calender/v3/primary/events?timeMin={datetime.datetime(date.year, date.month, 1,0,0,0)}&timeMax={date.year, date.month, 29,23,59,59}",
+        timeout=(3.0, 7.5),
+        headers={'Authorization': f"Bearer {access_token}"}
+    )
+    print(res.json())
+    return res.json()
