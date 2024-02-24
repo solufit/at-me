7749a4beae0d1d25ac416ebbb9b31a5fb73e41bd
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-02-25 05:04:35 +0900

    feature: アクセストークン管理機能の実装

--- auth/app/routers/oauth.py
+++ auth/app/routers/oauth.py
@@ -6,6 +6,8 @@ import os
 import time
 import redis
 import uuid
+import json
+import requests
 
 router = APIRouter()
 
@@ -26,7 +28,7 @@ rc = redis.StrictRedis(connection_pool=connection_pool)
 
 @router.get("/oauth2/login")
 async def login_form(redirect: str):
-    return f"{AUTHORIZATION_URL}?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope=openid%20email%20profile%20https://www.googleapis.com/auth/calendar&access_type=offline&prompt=consent"
+    return f"{AUTHORIZATION_URL}?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}/oauth2/login/callback&scope=openid%20email%20profile%20https://www.googleapis.com/auth/calendar&access_type=offline&prompt=consent"
 
 @router.get("/oauth2/login/callback/")
 async def login_callback(code: str = Query(...)):
@@ -59,7 +61,57 @@ async def login_callback(code: str = Query(...)):
         {
             "access_token":token_response_json["access_token"],
             "refresh_token":token_response_json["refresh_token"],
-            "exp":int(time.time())
+            "exp":int(time.time())+3600
         }
     )
     return RedirectResponse(url=f"{FRONTEND_URL}?jwt={token_response_json['id_token']}&linkcode={linkcode}")
+
+@router.get('/oauth2/token')
+async def get_token(linkcode: str, secure: str) -> str:
+    if secure != os.getenv('SECURE_LOCK'):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN
+        )
+    rc = redis.Redis(
+        host="atme-auth-redis",
+        port=6379,
+        db=0,
+    )
+    data = rc.hgetall(linkcode)
+    token = json.dumps({key.decode(): value.decode() for key, value in data.items()})
+    token = json.loads(token)
+    if int(token["exp"]) - int(time.time()) < 300:
+        async with httpx.AsyncClient() as client:
+            token_response = await client.post(
+                TOKEN_URL,
+                data={
+                    "grant_type": "refresh_token",
+                    "refresh_token": token['refresh_token'],
+                    "client_id": CLIENT_ID,
+                    "client_secret": CLIENT_SECRET,
+                },
+            )
+        token_response_json = token_response.json()
+        if token_response.is_error:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=token_response_json,
+            )
+    
+        linkcode=  str(uuid.uuid4())
+        rc = redis.Redis(
+            host="atme-auth-redis",
+            port=6379,
+            db=0,
+        )
+        rc.hmset(
+            linkcode,
+            {
+                "access_token":token_response_json["access_token"],
+                "refresh_token":token['refresh_token'],
+                "exp":int(time.time()) + 3600
+            }
+        )
+        return token_response_json["access_token"]
+    else:
+        return token['access_token']
