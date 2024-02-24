8b1714d26b811e6886df4bff413c8c932a249dea
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-02-24 14:17:28 +0900

    feature: タスクリスケ用のUI実装

--- frontend/components/uiparts/tasks.vue
+++ frontend/components/uiparts/tasks.vue
@@ -1,6 +1,7 @@
 import type { PartsTasks } from '#build/components';
 <script setup lang="ts">
 	import type { Task } from '~/types/task';
+	import { Calendar, DatePicker } from 'v-calendar';
 	const props = defineProps<{
 		tasks: Task[];
 	}>();
@@ -31,15 +32,21 @@ import type { PartsTasks } from '#build/components';
 						<div class="p-1">{{ task.note }}</div>
 					</div>
 					<div class="w-9">
-						<button class="bg-neutral" aria-label="スケジュールの変更">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-primary">
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
-								/>
-							</svg>
-						</button>
+						<div class="dropdown dropdown-end absolute">
+							<button tabindex="0" role="button" class="bg-neutral" aria-label="スケジュールの変更">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-primary">
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+									/>
+								</svg>
+							</button>
+							<div class="dropdown-content z-[1] menu p-2 shadow border-2 border-primary bg-base-100 rounded-box">
+								<div class="mb-2 flex items-center justify-center text-center font-semibold text-md">リスケジュール</div>
+								<DatePicker :min-date="new Date()" :value="task.due" mode="dateTime" color="green" borderless />
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
