6b91767a6224279fdf211af6658f5ba5d428f3fc
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-02-27 01:49:16 +0900

    feature: 設定画面にアカウント連携UIを追加

--- frontend/pages/setting.vue
+++ frontend/pages/setting.vue
@@ -26,8 +26,29 @@
 		<h1 class="font-bold text-2xl mb-12">設定</h1>
 		<div class="flex border-2 shadow-sm rounded-lg p-6 items-center justify-center h-32">
 			<div class="avater">
-				<div class="w-24 rounded-full">
+				<div class="w-12 md:w-24 rounded-full">
 					<img alt="User Icon" :src="user?.photoURL" class="rounded-full" />
+				</div>
+				<div class="block md:hidden items-center justify-center ml-11">
+					<div class="rounded-full" v-if="userLink?.provider == 'github'">
+						<svg viewBox="0 0 98 98" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4">
+							<path
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+								fill="#24292f"
+							/>
+						</svg>
+					</div>
+					<div class="rounded-full" v-if="userLink?.provider == 'google'">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-4 w-4">
+							<path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
+							<path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
+							<path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
+							<path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" />
+							<path d="M1 1h22v22H1z" fill="none" />
+						</svg>
+					</div>
 				</div>
 			</div>
 			<div class="md:w-1/2 p-3 ml-6">
@@ -36,8 +57,8 @@
 					<div class="text-sm m-1">{{ user?.email }}</div>
 				</div>
 			</div>
-			<div class="items-center justify-center mr-3">
-				<div class="w-12 rounded-full" v-if="userLink?.provider == 'github'">
+			<div class="hidden md:block items-center justify-center mr-6">
+				<div class="rounded-full" v-if="userLink?.provider == 'github'">
 					<svg viewBox="0 0 98 98" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8">
 						<path
 							fill-rule="evenodd"
@@ -47,7 +68,7 @@
 						/>
 					</svg>
 				</div>
-				<div class="w-12 rounded-full" v-if="userLink?.provider == 'google'">
+				<div class="rounded-full" v-if="userLink?.provider == 'google'">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-8 w-8">
 						<path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
 						<path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
@@ -68,66 +89,158 @@
 				<span class="ml-3">ログアウト</span>
 			</button>
 		</div>
-		<div class="mt-3 p-6">
-			<h2 class="font-bold">ワークタイム</h2>
-			<p class="m-2 text-sm text-gray-600">ワークタイムを設定するとワークタイム以外の時間にはタスクをレコメンドされなくなります。</p>
-			<div class="p-3">
-				<div class="md:flex flex-wrap items-center justify-center gap-3 my-4 mb-12">
-					<div class="form-control">
-						<label class="cursor-pointer label">
-							<span class="label-text mr-2">月曜日</span>
-							<input type="checkbox" v-model="wt_mon" class="checkbox checkbox-primary" />
-						</label>
-					</div>
-					<div class="form-control">
-						<label class="cursor-pointer label">
-							<span class="label-text mr-2">火曜日</span>
-							<input type="checkbox" v-model="wt_the" class="checkbox checkbox-primary" />
-						</label>
-					</div>
-					<div class="form-control">
-						<label class="cursor-pointer label">
-							<span class="label-text mr-2">水曜日</span>
-							<input type="checkbox" v-model="wt_wed" class="checkbox checkbox-primary" />
-						</label>
-					</div>
-					<div class="form-control">
-						<label class="cursor-pointer label">
-							<span class="label-text mr-2">木曜日</span>
-							<input type="checkbox" v-model="wt_thu" class="checkbox checkbox-primary" />
-						</label>
-					</div>
-					<div class="form-control">
-						<label class="cursor-pointer label">
-							<span class="label-text mr-2">金曜日</span>
-							<input type="checkbox" v-model="wt_fri" class="checkbox checkbox-primary" />
-						</label>
-					</div>
-					<div class="form-control">
-						<label class="cursor-pointer label">
-							<span class="label-text mr-2">土曜日</span>
-							<input type="checkbox" v-model="wt_sat" class="checkbox checkbox-primary" />
-						</label>
-					</div>
-					<div class="form-control">
-						<label class="cursor-pointer label">
-							<span class="label-text mr-2">日曜日</span>
-							<input type="checkbox" v-model="wt_sun" class="checkbox checkbox-primary" />
-						</label>
-					</div>
-				</div>
-				<div class="p-3 md:flex">
-					<div class="w-full md:w-1/2 p-2 md:p-4 fonm-control">
-						<label for="wt_start">開始時刻</label>
-						<input class="input w-full my-1" name="wt_start" type="time" />
-					</div>
-					<div class="w-full md:w-1/2 p-2 md:p-4 fonm-control">
-						<label for="wt_end">終了時刻</label>
-						<input class="input w-full my-1" name="wt_end" type="time" />
+		<div class="mt-3">
+			<div class="border rounded-lg p-2 my-6">
+				<h2 class="font-bold m-2">連携アカウント</h2>
+				<p class="m-4 text-sm text-gray-600">ワークタイムを設定するとワークタイム以外の時間にはタスクをレコメンドされなくなります。</p>
+				<div class="p-3">
+					<div class="overflow-x-auto">
+						<table class="table items-center justify-center text-center">
+							<thead>
+								<tr>
+									<th>アカウント</th>
+									<th>プロバイダー</th>
+									<th>カレンダーリソース</th>
+									<th>タスクリソース</th>
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<div class="flex items-center gap-3">
+											<div class="avatar">
+												<div class="mask mask-squircle w-12 h-12">
+													<img :src="user?.photoURL" alt="Avatar Tailwind CSS Component" />
+												</div>
+											</div>
+											<div>
+												<div class="font-bold text-left">{{ user?.displayName }}</div>
+												<div class="text-sm opacity-50">{{ user?.email }}</div>
+											</div>
+										</div>
+									</td>
+									<td class="flex items-center justify-center">
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-8 w-8">
+											<path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
+											<path
+												d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+												fill="#34A853"
+											/>
+											<path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
+											<path
+												d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+												fill="#EA4335"
+											/>
+											<path d="M1 1h22v22H1z" fill="none" />
+										</svg>
+										<span class="ml-3 text-lg">Google</span>
+									</td>
+									<td>Google Calender</td>
+									<td>Google Tasks</td>
+									<th>
+										<button class="btn btn-sm">連携解除</button>
+									</th>
+								</tr>
+								<tr>
+									<td>
+										<div class="flex items-center gap-3">
+											<div class="avatar">
+												<div class="mask mask-squircle w-12 h-12">
+													<img :src="user?.photoURL" alt="Avatar Tailwind CSS Component" />
+												</div>
+											</div>
+											<div>
+												<div class="font-bold text-left">{{ user?.displayName }}</div>
+												<div class="text-sm opacity-50">sasanqu_a</div>
+											</div>
+										</div>
+									</td>
+									<td class="flex items-center justify-center">
+										<svg viewBox="0 0 98 98" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8">
+											<path
+												fill-rule="evenodd"
+												clip-rule="evenodd"
+												d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+												fill="#24292f"
+											/>
+										</svg>
+										<span class="ml-3 text-lg">Github</span>
+									</td>
+									<td></td>
+									<td>Assigned Issue</td>
+									<th>
+										<button class="btn btn-sm">連携する</button>
+									</th>
+								</tr>
+							</tbody>
+						</table>
 					</div>
 				</div>
 			</div>
-			<button class="btn btn-primary w-full">保存</button>
+			<div class="border rounded-lg p-2 my-6 md:p-6">
+				<h2 class="font-bold m-2">ワークタイム</h2>
+				<p class="m-4 text-sm text-gray-600">ワークタイムを設定するとワークタイム以外の時間にはタスクをレコメンドされなくなります。</p>
+				<div class="p-3">
+					<div class="md:flex flex-wrap items-center justify-center gap-3 my-4 mb-12">
+						<div class="form-control">
+							<label class="cursor-pointer label">
+								<span class="label-text mr-2">月曜日</span>
+								<input type="checkbox" v-model="wt_mon" class="checkbox checkbox-primary" />
+							</label>
+						</div>
+						<div class="form-control">
+							<label class="cursor-pointer label">
+								<span class="label-text mr-2">火曜日</span>
+								<input type="checkbox" v-model="wt_the" class="checkbox checkbox-primary" />
+							</label>
+						</div>
+						<div class="form-control">
+							<label class="cursor-pointer label">
+								<span class="label-text mr-2">水曜日</span>
+								<input type="checkbox" v-model="wt_wed" class="checkbox checkbox-primary" />
+							</label>
+						</div>
+						<div class="form-control">
+							<label class="cursor-pointer label">
+								<span class="label-text mr-2">木曜日</span>
+								<input type="checkbox" v-model="wt_thu" class="checkbox checkbox-primary" />
+							</label>
+						</div>
+						<div class="form-control">
+							<label class="cursor-pointer label">
+								<span class="label-text mr-2">金曜日</span>
+								<input type="checkbox" v-model="wt_fri" class="checkbox checkbox-primary" />
+							</label>
+						</div>
+						<div class="form-control">
+							<label class="cursor-pointer label">
+								<span class="label-text mr-2">土曜日</span>
+								<input type="checkbox" v-model="wt_sat" class="checkbox checkbox-primary" />
+							</label>
+						</div>
+						<div class="form-control">
+							<label class="cursor-pointer label">
+								<span class="label-text mr-2">日曜日</span>
+								<input type="checkbox" v-model="wt_sun" class="checkbox checkbox-primary" />
+							</label>
+						</div>
+					</div>
+					<div class="p-3 md:flex">
+						<div class="w-full md:w-1/2 p-2 md:p-4 fonm-control">
+							<label for="wt_start">開始時刻</label>
+							<input class="input w-full my-1" name="wt_start" type="time" />
+						</div>
+						<div class="w-full md:w-1/2 p-2 md:p-4 fonm-control">
+							<label for="wt_end">終了時刻</label>
+							<input class="input w-full my-1" name="wt_end" type="time" />
+						</div>
+					</div>
+				</div>
+				<div class="w-full text-center">
+					<button class="btn btn-primary w-full max-w-md">保存</button>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
