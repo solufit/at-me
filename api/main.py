5003685e20b85af983b94ddfabc222e9eaedcd75
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-02-26 09:41:04 +0900

    fix

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 import uvicorn 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.v1.api import api_router
 from app.core import config
@@ -13,16 +13,10 @@ app = FastAPI(
 # Set all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://atme.solufit.net","http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 ),
 
 app.include_router(api_router, prefix=config.API_V1_STR)
-
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", port=8080, host="0.0.0.0",reload = True)
-    
