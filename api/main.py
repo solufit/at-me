f7a5516f1186167f269399ece14ee30c596aadff
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-02-26 09:25:02 +0900

    fix

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 import uvicorn 
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.api import api_router
 from app.core import config
