0ca438b3951b8a5a21f5371f2a5baff39031014c
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-02-26 19:10:55 +0900

    githubログイン機能の仮作成

--- auth/app/routers/endpoints/github.py
+++ auth/app/routers/endpoints/github.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Request, Depends, HTTPException, status, Query
-from fastapi.security import OAuth2AuthorizationCodeBearer
+from fastapi import APIRouter, HTTPException, status, Query
 from fastapi.responses import RedirectResponse
 import httpx
 import os
@@ -7,17 +6,14 @@ import time
 import redis
 import uuid
 import json
-import requests
-
+from auth.app.core import config
 router = APIRouter()
 
 # 環境変数の読み込み
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-REDIRECT_URI = os.getenv("REDIRECT_URI")
-FRONTEND_URL = os.getenv("FRONTEND_URL")
-AUTHORIZATION_URL = "https://accounts.google.com/o/oauth2/v2/auth"
-TOKEN_URL = "https://oauth2.googleapis.com/token"
+CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
+CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
+AUTHORIZATION_URL = "https://github.com/login/oauth/authorize"
+TOKEN_URL = "https://github.com/login/oauth/access_token"
 
 connection_pool = redis.ConnectionPool(
     host="atme_auth_redis",
@@ -26,19 +22,18 @@ connection_pool = redis.ConnectionPool(
 )
 rc = redis.StrictRedis(connection_pool=connection_pool)
 
-@router.get("/oauth2/login")
-async def login_form(redirect: str):
-    return f"{AUTHORIZATION_URL}?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}/oauth2/login/callback&scope=openid%20email%20profile%20https://www.googleapis.com/auth/calendar&access_type=offline&prompt=consent"
+@router.get("/login")
+async def login_form():
+    return f"{AUTHORIZATION_URL}?client_id={CLIENT_ID}&redirect_uri={config.AUTH_HOST}/github/callback&scope=user:read"
 
-@router.get("/oauth2/login/callback/")
+@router.get("/callback")
 async def login_callback(code: str = Query(...)):
     async with httpx.AsyncClient() as client:
         token_response = await client.post(
             TOKEN_URL,
             data={
-                "grant_type": "authorization_code",
                 "code": code,
-                "redirect_uri": f"{REDIRECT_URI}/oauth2/login/callback",
+                "redirect_uri": f"{config.AUTH_HOST}/github/callback",
                 "client_id": CLIENT_ID,
                 "client_secret": CLIENT_SECRET,
             },
@@ -61,12 +56,13 @@ async def login_callback(code: str = Query(...)):
         {
             "access_token":token_response_json["access_token"],
             "refresh_token":token_response_json["refresh_token"],
-            "exp":int(time.time())+3600
+            "exp":int(time.time()) + int(token_response_json["expires_in"]),
+            "exp_refresh": int(time.time()) + int(token_response_json["refresh_token_expires_in"])
         }
     )
-    return RedirectResponse(url=f"{FRONTEND_URL}?jwt={token_response_json['id_token']}&linkcode={linkcode}")
+    return RedirectResponse(url=f"{config.FRONTEND_URL}?linkcode={linkcode}")
 
-@router.get('/oauth2/token')
+@router.get('/token')
 async def get_token(linkcode: str, secure: str) -> str:
     if secure != os.getenv('SECURE_LOCK'):
         raise HTTPException(
@@ -80,6 +76,10 @@ async def get_token(linkcode: str, secure: str) -> str:
     data = rc.hgetall(linkcode)
     token = json.dumps({key.decode(): value.decode() for key, value in data.items()})
     token = json.loads(token)
+    if "access_token" in token:
+        raise HTTPException(status_code=401, detail="LinkCode is invalid")
+    if int(token["exp"]) - int(time.time()) < 300:
+        raise HTTPException(status_code=401,detail="Refresh Token is expired")
     if int(token["exp"]) - int(time.time()) < 300:
         async with httpx.AsyncClient() as client:
             token_response = await client.post(
@@ -108,8 +108,9 @@ async def get_token(linkcode: str, secure: str) -> str:
             linkcode,
             {
                 "access_token":token_response_json["access_token"],
-                "refresh_token":token['refresh_token'],
-                "exp":int(time.time()) + 3600
+                "refresh_token":token_response_json['refresh_token'],
+                "exp":int(time.time()) + int(token_response_json["expires_in"]), # アクセストークンの有効期限が切れるまでの時間
+                "exp_refresh": int(time.time()) + int(token_response_json["refresh_token_expires_in"])
             }
         )
         return token_response_json["access_token"]
