f359079c2e5d70e047729e2bdc8c72ea9be86393
Yuta Takahashi <y.highbridge04@gmail.com>
2024-02-28 16:38:51 +0900

    fix test code for create and delete

--- api/tests/test_db/test_dbtask.py
+++ api/tests/test_db/test_dbtask.py
@@ -72,7 +72,8 @@ default_task_nearline : models.Task = models.Task(
 
 target_task : models.Task = models.Task(
     localId = "localId",
-    id = "targetid",
+    id = "targetid" ,
+
     title = "title",
     note = "notes",
     updated = datetime.datetime(1, 2, 1, 1, 1, 1),
@@ -128,57 +129,62 @@ class db_task_Tests():
         assert result["id"] == Task.from_orm(default_task)
 
     def test_task_create_delete(self, db):
+ 
 
-        
-        with db() as session:
-            session : Session = session # for completion
-
-            task = task_db(
-                session = session,
+        task = task_db(
+            sessionmk= db
+            
+        )
+        task.create(
+            localId = target_task.localId, 
+            id = target_task.id,
+            title = target_task.title,
+            note = target_task.note,
+            updated = target_task.updated,
+            selfLink = target_task.selfLink,
+            parent = target_task.parent,
+            position = target_task.position,
+            status = target_task.status,
+            due = target_task.due,
+            completed = target_task.completed,
+            deleted = target_task.deleted,
+            hidden = target_task.hidden,
+            kind=target_task.kind,
+            etag = target_task.etag           
                 
-            )
-            task.create(
-                localId = target_task.localId, 
-                id = target_task.id,
-                title = target_task.title,
-                note = target_task.note,
-                updated = target_task.updated,
-                selfLink = target_task.selfLink,
-                parent = target_task.parent,
-                position = target_task.position,
-                status = target_task.status,
-                due = target_task.due,
-                completed = target_task.completed,
-                deleted = target_task.deleted,
-                hidden = target_task.hidden
-                 
-            )
+        )
 
-            result = task.load(
-                localId = target_task.localId
-            )
+        result = task.load(
+            localId = target_task.localId
+        )
 
-            assert result["id"] == Task.from_orm(default_task)
-            assert ["targetid"] == Task.from_orm(target_task)
+        assert result["id"] == Task.from_orm(default_task)
+        assert result["targetid"] == Task.from_orm(target_task)
 
-            task.delete(
-                id = 1,
-                localId = default_task.localId
-            )
+        task.delete(
+            id = default_task.id,
+            localId = default_task.localId
+        )
 
-            with pytest.raises(KeyError):
-                result["id"] == Task.from_orm(default_task)
+        result = task.load(
+            localId = target_task.localId
+        )
+        with pytest.raises(KeyError):
+            result["id"] == Task.from_orm(default_task)
 
-            assert result["targetid"] == Task.from_orm(target_task)
+        assert result["targetid"] == Task.from_orm(target_task)
 
-            task.delete(
-                id = 2,
-                localId = target_task.localId
-            )
+        task.delete(
+            id = target_task.id,
+            localId = target_task.localId
+        )
 
-            with pytest.raises(KeyError):
-                result["targetid"] == Task.from_orm(target_task)
-    
+        result = task.load(
+            localId = target_task.localId
+        )
+        with pytest.raises(KeyError):
+            result["targetid"] == Task.from_orm(target_task)
+
     
     def test_task_update(self, db):
         with db() as session:
@@ -201,7 +207,9 @@ class db_task_Tests():
                 due = default_task_updated.due,
                 completed = default_task_updated.completed,
                 deleted = default_task_updated.deleted,
-                hidden = default_task_updated.hidden
+                hidden = default_task_updated.hidden,
+                etag = default_task_updated.etag,
+                kind = default_task_updated.kind
 
             )
 
@@ -209,7 +217,7 @@ class db_task_Tests():
                 localId = default_task.localId
             )
 
-            assert result[1] == Task.from_orm(default_task_updated)
+            assert result[default_task.id] == Task.from_orm(default_task_updated)
 
     def test_get_deadline_tasks(self, db):
         with db() as session:
