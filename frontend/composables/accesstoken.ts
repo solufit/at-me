f2065603e476a58654e70719fdd6f0baeb0405a8
sasanqu_a <78304794+sasanqua-create@users.noreply.github.com>
2024-02-29 00:39:34 +0900

    bugfix: トークンの初期値が文字列である問題の修正

--- frontend/composables/accesstoken.ts
+++ frontend/composables/accesstoken.ts
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 export const useAccessToken = defineStore(
 	'token',
 	() => {
-		const token = ref<string | null>('');
+		const token = ref<string | null>(null);
 		function setToken(newuser: string) {
 			token.value = newuser;
 		}
